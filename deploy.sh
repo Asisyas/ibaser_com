7d2c042586a21040ac974e219cf152f9b0957d61
Stanislau Komar <stanislau_komar@epam.com>
2019-03-18 16:04:37 +0300

    Fix travis

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 rm -rf $THEME_ROOT_PATH/node_modules $THEME_ROOT_PATH/sass
-tar -czvf ibaser.tar.gz $THEME_ROOT_PATH
-sshpass -e scp -r $THEME_ROOT_PATH/* $DEPLOY_USER@$DEPLOY_HOST:$THEME_PATH_LIVE/ibaser
+# tar -czvf ibaser.tar.gz $THEME_ROOT_PATH
+sshpass -e scp -rp $THEME_ROOT_PATH/* $DEPLOY_USER@$DEPLOY_HOST:$THEME_PATH_LIVE/ibaser
