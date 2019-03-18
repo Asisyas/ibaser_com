8a01a2d3b06a4c46c6126f41f961b23e372951c3
Stanislau Komar <stanislau_komar@epam.com>
2019-03-18 16:17:11 +0300

    Fix travis

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 rm -rf $THEME_ROOT_PATH/node_modules $THEME_ROOT_PATH/sass
-sshpass -e scp -r $THEME_ROOT_PATH/* $DEPLOY_USER@$DEPLOY_HOST:$THEME_PATH_LIVE/ibaser --overwrite
+sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST < /bin/bash "rm -rf $THEME_PATH_LIVE/ibaser/*"
+sshpass -e scp -r $THEME_ROOT_PATH/* $DEPLOY_USER@$DEPLOY_HOST:$THEME_PATH_LIVE/ibaser
