9ffa87b5f921867c64fc9607a03412bc4e4ac9a0
Stanislau Komar <stanislau_komar@epam.com>
2019-03-18 08:03:11 +0300

    Add travis

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 rm -rf $THEME_ROOT_PATH/node_modules $THEME_ROOT_PATH/sass
 tar -czvf ibaser.tar.gz $THEME_ROOT_PATH
 sshpass -e scp ibaser.tar.gz $DEPLOY_USER@$DEPLOY_HOST:~/
-sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST ~/deploy.sh
+sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST "/bin/bash deploy.sh"
