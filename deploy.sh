c82f3808612362148eb093aa7b3548ce450c5e7d
Stanislau Komar <stanislau_komar@epam.com>
2019-03-22 02:22:01 +0300

    Update travis

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 rm -rf $THEME_ROOT_PATH/node_modules $THEME_ROOT_PATH/sass
-sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST < /bin/bash "rm -rf $THEME_PATH_LIVE/ibaser/*"
-sshpass -e scp -r $THEME_ROOT_PATH/* $DEPLOY_USER@$DEPLOY_HOST:$THEME_PATH_LIVE/ibaser
+sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST < /bin/bash "rm -rf $THEME_PATH_LIVE/*"
+sshpass -e scp -r $THEME_ROOT_PATH/* $DEPLOY_USER@$DEPLOY_HOST:$THEME_PATH_LIVE/
 sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST < /bin/bash "wp cache flush --path=$WP_ROOT_LIVE"
